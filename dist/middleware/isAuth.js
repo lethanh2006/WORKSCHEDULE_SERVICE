79991ffe5e6d2e65c8b755664c8e6ef2bc4a081f
lethanh2006 <thanhld2006.ptit@gmail.com>
2026-08-20 18:05:42 +0700

    Ngừng theo dõi mã build trong dist

--- dist/middleware/isAuth.js
+++
@@ -1,9 +0,0 @@
-export const isAuth = (req, res, next) => {
-    const payload = req.headers['x-user-payload'];
-    if (!payload) {
-        res.status(401).json({ message: "Unauthorized" });
-        return;
-    }
-    req.user = JSON.parse(Buffer.from(payload, 'base64').toString('utf8'));
-    next();
-};
